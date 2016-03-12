a4dc3ddf6f12f017fde59f6fc728d63a45f92132
dailyherald <tbroderick@dailyherald.com>
2016-03-12 09:05:33 -0700

    Adds Detail page

--- app.py
+++ app.py
@@ -24,6 +24,16 @@ def index():
     object_list = get_csv('./static/la-riots-deaths.csv')
     return render_template(template, object_list=object_list)
 
+
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv('./static/la-riots-deaths.csv')
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+
+
 # Sets up the server on localhost:5000
 if __name__ == '__main__':
     # Fire up the Flask test server
